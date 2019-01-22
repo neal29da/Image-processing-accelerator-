443a8941ac03b5d8994426e490edd7664fa23613
Neal DAHAN <neal.dahan@nightle.fr>
2019-01-22 14:38:44 +0200

    a new version with an second file output that return to the line every 4 bytes

--- Script/bmp_to_hexa.py
+++ Script/bmp_to_hexa.py
@@ -1,15 +1,20 @@
 import binascii
 
+from os import system
 
 #   input file bmp image
 bmp_file = '/Users/nealdahan/Desktop/testttt/im_3.bmp'
+output_file1 = '/Users/nealdahan/Desktop/testttt/bb.txt'
+
+# output file with return to line every 4 byte
+output_file2 = '/Users/nealdahan/Desktop/testttt/hexa_output_4B.txt'
 
 #   Opening bmp image
 with open(bmp_file, 'rb') as f:
     content = f.read()
 
 #   Create output data, need to specify a location for output file
-hexa_file = open('/Users/nealdahan/Desktop/testttt/bb.txt', "w")
+hexa_file = open(output_file1, "w")
 
 # Converting to hexa
 data = str(binascii.hexlify(content))
@@ -25,3 +30,8 @@ hexa_file.write(new_data)
 
 # Close File
 hexa_file.close()
+linux_command = """a=$(fold -w4 """ + output_file1 + """); echo "$a" > """ + output_file2
+
+system(linux_command)
+
+system("ls /Users/nealdahan/Desktop/testttt")
