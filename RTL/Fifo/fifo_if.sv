4f7f3760382a5588691444db4c0915504be499d6
Neal DAHAN <neal.dahan@nightle.fr>
2019-01-22 15:33:09 +0200

    fix bug in modport

--- RTL/Fifo/fifo_if.sv
+++ RTL/Fifo/fifo_if.sv
@@ -23,14 +23,14 @@ modport DUT_MEM_ARR (
                     input       clk,
                     input       data_in,
                     output      data_out
-)
+);
 
 modport DUT_READ_POI (
                     input       clk,
                     input       rst_n,
                     input       rd,
-                    input       fifo_full,
-)
+                    input       fifo_full
+);
 
 modport DUT_STATUS  (
                     input       clk,
@@ -39,12 +39,12 @@ modport DUT_STATUS  (
                     input       wr,
                     output      fifo_full,
                     output      fifo_empty
-)
+);
 modport DUT_WR_POI (
                     input       clk,
                     input       wr,
                     input       rst_n,
                     input       fifo_full
 
-)
+);
 endinterface
