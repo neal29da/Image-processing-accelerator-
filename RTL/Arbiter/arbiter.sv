75f8639e0d26b1e66efee5166d60bf675dc7b590
Neal DAHAN <neal.dahan@nightle.fr>
2019-01-22 16:54:33 +0200

    fix bug according to mode != 0 for get a request.

--- RTL/Arbiter/arbiter.sv
+++ RTL/Arbiter/arbiter.sv
@@ -8,20 +8,23 @@ module arbiter(arbiter_if.DUT arb_if);
   logic slv0_ready;
   logic slv1_ready;
   
+  // if data0 = '1' and data
   
   always @ (posedge arb_if.clk or negedge arb_if.rst_n)
     if(! arb_if.rst_n) begin
        arb_if.slvx_mode=0;
-       arb_if.slv0_ready = 1;
+       arb_if.slvx_data_valid = 0;
+       arb_if.slv0_ready = 0;
        arb_if.slv1_ready = 0;
+       arb_if.slvx_proc_val = 0;
       end
-    else if ( arb_if.slv0_data_valid && ~ arb_if.fifo_full && ~ arb_if.mstr0_cmplt) begin
+    else if (arb_if.slv0_mode && arb_if.slv0_data_valid && ~ arb_if.fifo_full && ~ arb_if.mstr0_cmplt) begin
       arb_if.slvx_mode = arb_if.slv0_mode;
       arb_if.slvx_data_valid = arb_if.slv0_data_valid;
       arb_if.slvx_proc_val = arb_if.slv0_proc_valid;
       arb_if.slvx_data = arb_if.slv0_data;
     end
-    else if ( arb_if.slv1_data_valid && ~ arb_if.fifo_full && ~ arb_if.mstr0_cmplt) begin
+    else if (arb_if.slv1_mode && arb_if.slv1_data_valid && ~ arb_if.fifo_full && ~ arb_if.mstr0_cmplt) begin
       arb_if.slvx_mode = arb_if.slv1_mode;
       arb_if.slvx_data_valid = arb_if.slv1_data_valid;
       arb_if.slvx_proc_val = arb_if.slv1_proc_valid;
