d9edb8c8b79367f64e8bfc930d805b93639de43e
Neal DAHAN <neal.dahan@nightle.fr>
2019-01-22 13:20:30 +0200

    Final script converter

--- Script/bmp_to_hexa.py
+++ Script/bmp_to_hexa.py
@@ -1,21 +1,27 @@
 import binascii
 
 
-#   input file
+#   input file bmp image
 bmp_file = '/Users/nealdahan/Desktop/testttt/im_3.bmp'
 
+#   Opening bmp image
 with open(bmp_file, 'rb') as f:
     content = f.read()
 
-#   Output file
-hexa_file = open('/Users/nealdahan/Desktop/testttt/bb.txt', "a")
+#   Create output data, need to specify a location for output file
+hexa_file = open('/Users/nealdahan/Desktop/testttt/bb.txt', "w")
 
+# Converting to hexa
 data = str(binascii.hexlify(content))
 data = data.lstrip("b'")
-data = data[0:108] + '0000000000000000' + data[108:]
 
+# Addding 16 bit to zero between the HEADERS (54bytes) and the rest of Data
+new_data = data[0:432]
+new_data = new_data + '0000000000000000'
+new_data = new_data + data[432:]
 
-hexa_file.write(data)
+# Write into file
+hexa_file.write(new_data)
 
-
+# Close File
 hexa_file.close()
