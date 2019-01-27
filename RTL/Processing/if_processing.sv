183211adaf968ef4006328fb171e041568f2b86e
Irina <irinazern@gmail.com>
2019-01-27 15:54:02 +0200

    Bug fixed, added padding

--- RTL/Processing/if_processing.sv
+++ RTL/Processing/if_processing.sv
@@ -1,10 +1,12 @@
-interface if_processing(input bit clk, rst_n);
+interface if_processing(input bit clk);
+
+    bit rst_n;
     
-    logic [1:0] slvx_mode;
-    bit slvx_data_valid, fifo_empty, wr, mstr_data_cmplt;
-    logic [7:0] slvx_proc_val;
-    logic [D_WIDTH - 1 : 0] slvx_data, data_out;
+  logic [1:0] slvx_mode;
+    bit slvx_data_valid, fifo_empty, wr, mstr0_cmplt;
+  logic [7:0] slvx_proc_val;
+  logic [DW - 1 : 0] slvx_data, data_fifo;
         
-  modport DUT(input rst_n, clk, slvx_data_valid, fifo_empty, slvx_mode, slvx_proc_val,slvx_data, output wr, mstr_data_cmplt, data_out );
+  modport DUT_PROCESSING(input rst_n, clk, slvx_data_valid, fifo_empty, slvx_mode, slvx_proc_val,slvx_data, output wr, mstr0_cmplt, data_fifo );
 
 endinterface
